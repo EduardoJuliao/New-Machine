8ad3f63c2bc40416fe601d84fcc4a76a629c4091
Eduardo Julião Junior <eduardo.juliaojr@hotmail.com>
2020-10-20 23:22:57 +0100

    Forgot to uncomment....

--- Machine.ps1
+++ Machine.ps1
@@ -17,18 +17,18 @@ Import-Module $PSScriptRoot\lib\IIS.config.psm1
 Import-Module $PSScriptRoot\lib\Programs\ssms.setup.psm1
 
 # Base
-# Set-Folders
-# Set-PSCommands
-# Set-Choco
+Set-Folders
+Set-PSCommands
+Set-Choco
 
-# # Enviornment
-# Set-DotnetCore31 
-# Set-Git
-# Set-Node
-# Set-NpmPackages
-# Set-SqlServer
-# Update-IIS
+# Enviornment
+Set-DotnetCore31 
+Set-Git
+Set-Node
+Set-NpmPackages
+Set-SqlServer
+Update-IIS
 
-# # Programs
-# Set-SSMS
-# Set-AdditionalPrograms
+# Programs
+Set-SSMS
+Set-AdditionalPrograms
